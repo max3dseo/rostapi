2b5937fb552f000c9c3609e0627102c15acf85aa
Fish <fish-seo@test.com>
2022-04-11 14:05:07 +0300

    test devapi4

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-test api
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Test JS</title>
+	</head>
+	<body>
+	
+	
+	
+<script>
+"use strict";
+
+
+
+
+
+
+
+
+
+
+</script>	
+	
+	
+	
+		
+		test api !!!
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	</body>
+</html>
